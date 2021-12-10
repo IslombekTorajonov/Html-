111c1026861c0b3fc857add51f523acab44f11e6
developer <90696696+IslombekTorajonov@users.noreply.github.com>
2021-12-10 20:28:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,123 @@
+  // 1 savol
+  // function rend_nums(){
+  //   ls = []
+  //   for(i = 0;i < 10; i++){
+  //     ls.push(Math.round(Math.random()*50))
+  //   }
+  // return ls
+  // }
+  // console.log(rend_nums())  
+
+
+
+  // 2-savol
+  // function max_nm(ls){
+  //   return Math.max.apply(null, ls)
+  // }
+  // console.log(max_nm([2,3,5,69,9]))
+
+
+  // 3-savol
+  // function counter(ls){
+  //   counter = 0
+  //   for(i of ls){
+  //     for(x of i){
+  //       if(x === 't'){
+  //         counter += 1
+  //       }
+  //     }
+  //   }
+  //   return counter
+  // }
+  // console.log(counter(['JavaScript', 'Docker', 'GitHub', 'BitBucket']))
+
+  // 4-savol
+  // for(let x=1; x <= 40; x++){
+//  if(x % 2 ==0){
+//    console.log(x)
+//  }
+// }
+
+  // 5-savol
+//   for(let x=1; x <= 90; x++){
+//  if(x % 5 ==0){
+//    console.log(x)
+//  }
+// }
+
+  // 6-savol
+  // function sorter(item){
+  //   let obj = { nums:[], strs:[],}
+  //   for(i=0; i < item.length; i++){
+  //     if(parseInt(item[i])){
+  //       obj['nums'].push(i)
+  //     }else{
+  //       obj['strs'].push(item[i])
+  //     }
+  //   } 
+  //   return obj
+  // }
+  // console.log(sorter('hel2ll2o'))
+
+  // 7-savol
+  // function q7(item){
+  //   let first = item.slice(0,4)
+  //   let second = item.slice(-2)
+  //   console.log(second + first)
+  // }
+  // q7('ananas')
+
+
+  // 8-savol
+  // function prm(){
+  //   fruit  = null
+  //   while(fruit !== 'stop'){
+  //     fruit = prompt('enter >>>  ')
+  //   }
+  // }
+  // prm()
+
+  // 10-savol
+  function username(username){
+    username = prompt('username')
+    if(username === 'gandi123'){
+      alert('Welcome')
+    }else{
+      alert('try again')
+    }
+  }
+  username()
+  
+
+  //11-savol
+ 
+// let usr = prompt("son kiriting");
+//     let oqk= ['A2','A4','A6','A8','B1','B3','B5','B7','C2','C4','C6','C8','D1','D3','D5','D7','E2','E4','E6','E8','F1','F3','F5','F7','G2','G4','G6','G8','H1','H3','H5','H7'];
+//   let qorak= ['A1','A3','A5','A7','B2','B4','B6','B8','C1','C3','C5','C7','D2','D4','D6','D8','E1','E3','E5','E7','F2','F4','F6','F8','G1','G3','G5','G7','H2','H4','H6','H8'];
+//   for(let i = 0; i < qorak.length; i++) {
+//     if(usr == qorak[i]){
+//       alert('qorak')
+//     }
+//   }
+//   for(let a = 0; a < oqk.length; a++) {
+//     if(oqk[a] == usr) {
+//       alert('oqk')
+//     }
+//   }
+
+
+                      // 14-savol
+// let arr = [0,1,2,3,4,5,6];
+
+// function ffun(){
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i]* 2) {
+//       alert(arr[i]*2)
+//     }
+//   }
+// }
+// ffun(arr)
+
+ 
+ 
+
